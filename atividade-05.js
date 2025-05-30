cddcd82d2d71cf69e4bb85588b668297a509ca95
Murilo <murilo.r.rocha@aluno.senai.br>
2025-05-30 11:46:24 -0300

    Remove o primeiro item

---
+++ atividade-05.js
@@ -0,0 +1,13 @@
+// 5 – Erro de importação
+
+let importados = [
+    "carregador portátil",
+    "cabo HDMI",
+    "leitor biométrico",
+    "controle sem fio",
+    "webcam 4K"
+  ];
+  
+  importados.shift()
+
+console.log(`Com o primeiro item removido: ${importados}`);
