7a0f08fcd2c807f6eb4f6083f38b41da61f4ed70
Murilo <murilo.r.rocha@aluno.senai.br>
2025-05-30 11:46:45 -0300

    Remove o primeiro item

---
+++ atividade-06.js
@@ -0,0 +1,13 @@
+// 6 – Primeira mensagem indevida
+
+let mensagens = [
+    "Propaganda indevida",
+    "Bom dia, posso ajudar?",
+    "Preciso de suporte técnico",
+    "Claro, qual o problema?",
+    "Erro ao acessar conta"
+  ];
+  
+  mensagens.shift()
+
+  console.log(`Com a mensagem removida: ${mensagens}`);
