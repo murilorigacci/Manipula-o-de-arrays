7506f684d76175db9574f81c70ca7c7d03e8967f
Murilo <murilo.r.rocha@aluno.senai.br>
2025-05-30 11:51:09 -0300

    Adiciona dois itens no começo do array

---
+++ atividade-08.js
@@ -0,0 +1,15 @@
+// 8 – Alunos transferidos
+
+let chamada = [
+    "Carlos",
+    "Fernanda",
+    "João",
+    "Mariana",
+    "Lucas",
+    "Camila"
+  ];
+  
+  
+  chamada.unshift(`Beatriz`, ` Rafaela`)
+
+console.log(`Depois de colocar as alunas na lista: ${chamada}`);
