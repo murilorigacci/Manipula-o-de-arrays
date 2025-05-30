38a8cb361c288f429aa64fea3389c1cf093f826e
Murilo <murilo.r.rocha@aluno.senai.br>
2025-05-30 11:37:54 -0300

    Adiciona 2 itens

---
+++ atividade-01.js
@@ -0,0 +1,16 @@
+// 1 – Cadastro de novos produtos
+
+let promocoes = [
+    "teclado mecânico",
+    "mouse gamer",
+    "monitor ultrawide",
+    "cadeira ergonômica",
+    "webcam HD",
+    "fone bluetooth",
+    "notebook i5",
+    "hub USB"
+  ];
+  
+  promocoes.push(`SSD 1TB`, `mesa digitalizadora`)
+  console.log(`Com os novos itens na lista:`, promocoes);
+
