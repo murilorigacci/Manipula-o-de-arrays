3f350fa9f5ebfd6175cd186fce37af6f68cae145
Murilo <murilo.r.rocha@aluno.senai.br>
2025-05-30 11:41:03 -0300

    Remove o último item

---
+++ atividade-04.js
@@ -0,0 +1,12 @@
+//  4 – Cancelamento de última inscrição
+
+let inscritos = [
+    "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
+  ];
+  
+
+let ultimaInscricaoRemovida = inscritos.pop()
+
+  console.log(`Iten removido: ${ultimaInscricaoRemovida}`);
+
+console.log(`Depois da Remover o item: ${inscritos}`);
