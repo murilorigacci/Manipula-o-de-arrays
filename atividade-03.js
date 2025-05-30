71e1914aa23cebeedd1071cd7c517d28499020d2
Murilo <murilo.r.rocha@aluno.senai.br>
2025-05-30 11:40:08 -0300

    Remove o último item

---
+++ atividade-03.js
@@ -0,0 +1,16 @@
+//  3 – Último produto com defeito
+
+let producao = [
+    "mousepad",
+    "placa de vídeo",
+    "fonte de alimentação",
+    "HD externo",
+    "notebook gamer",
+    "roteador"
+  ];
+  
+  let ultimoItemRemovido = producao.pop()
+
+  console.log(`Iten removido: ${ultimoItemRemovido}`);
+
+console.log(`Depois da Remover o item: ${producao}`);
