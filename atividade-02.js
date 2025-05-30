c40ea1836f3621b5aff2b3fd591224df03c85cf6
Murilo <murilo.r.rocha@aluno.senai.br>
2025-05-30 11:38:37 -0300

    Adiciona 2 itens

---
+++ atividade-02.js
@@ -0,0 +1,15 @@
+//  2 – Expansão de cardápio
+
+let cardapio = [
+    "pizza de calabresa",
+    "lasanha",
+    "sushi",
+    "salada Caesar",
+    "pastel",
+    "yakisoba",
+    "feijoada"
+  ];
+  
+  cardapio.push(`hambúrguer artesanal`, `wrap de frango`)
+
+  console.log(`Com os novos itens:`, cardapio);
